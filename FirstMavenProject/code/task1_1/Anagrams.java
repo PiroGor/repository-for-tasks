569842768864ff8a3961223e742854a2f7b14798
Anastasiia Nudha <nudhaa@outlook.com>
2023-12-10 20:14:53 +0100

    added methods for splitting
    added methods makeReverse() for separating words

--- FirstMavenProject/code/task1_1/Anagrams.java
+++ FirstMavenProject/code/task1_1/Anagrams.java
@@ -1,10 +1,22 @@
 package task1_1;
 
+import java.util.Scanner;
+
 public class Anagrams {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		String str=scan.nextLine();
+		
+	}
+	
+	public static void makeReverse(String str) {
+		String forSplit=" ";
+		String[] words=str.split(forSplit);
+		for(int i=0;i<words.length;i++) {
+			StringRevers.reverse(words[i]);
+		}
+		
 	}
 
 }
