fe83cdb32a14ed0128ff575d362090b827f4ba1e
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-13 17:39:13 +0100

    create StringRevers.java

---
+++ textRevers/src/main/java/app/textRevers/StringRevers.java
@@ -0,0 +1,27 @@
+package app.textRevers;
+
+public class StringRevers {
+	public static void reverse(String word) {
+		char[] charWord = word.toCharArray();
+		int startPoint = 0;
+		int endPoint = charWord.length - 1;
+		while (startPoint <= endPoint) {
+			while (startPoint < charWord.length - 1 && !Character.isLetter(charWord[startPoint])) {
+				startPoint++;
+			}
+			while (endPoint > 0 && !Character.isLetter(charWord[endPoint])) {
+				endPoint--;
+			}
+
+			if (startPoint <= endPoint) {
+				char temp = charWord[startPoint];
+				charWord[startPoint] = charWord[endPoint];
+				charWord[endPoint] = temp;
+				startPoint++;
+				endPoint--;
+			}
+		}
+		System.out.print(charWord);
+		System.out.print(" ");
+	}
+}
