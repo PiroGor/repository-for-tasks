1cf0ac8fee5a8baf5dfb02384b637f313d7bae61
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-15 15:13:50 +0100

    update Main.java
    replace splitStringToRevers method to Revers.java

--- textRevers/src/main/java/app/textRevers/Main.java
+++ textRevers/src/main/java/app/textRevers/Main.java
@@ -8,19 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(reverseYourString("qwe1rty5u rty888jnm!"));
+		System.out.println(Revers.splitStringToRevers("qwe1rty5u rty888jnm!"));
 
-	}
-
-	/*1.rename method from makeReverse to reverseYourString
-	2.make method which return String not just void
-	3.change logic a little bit
-	*  */
-	public static String reverseYourString(String str) {
-		final ArrayList<String> words=new ArrayList<>();
-		Collections.addAll(words, str.split(" "));
-		final ArrayList<String> splitWords=new ArrayList<>();
-		words.forEach(s-> splitWords.add(StringRevers.reverse(s)));
-        return String.join(" ",splitWords);
 	}
 }
