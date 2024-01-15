1f6baac007ec60dae3feb2fbd0e17a8601b1e840
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-15 15:12:55 +0100

    update Revers.java

--- textRevers/src/main/java/app/textRevers/Revers.java
+++ textRevers/src/main/java/app/textRevers/Revers.java
@@ -1,7 +1,21 @@
 package app.textRevers;
 
-public class StringRevers {				//
-	public static String reverse(String word) {		//chane the return type
+import java.util.*;
+
+public class Revers {				//rename class
+	/*1.rename method from makeReverse to splitStringToRevers
+	  2.make method which return String not just void
+	  3.change logic a little bit
+	  4.replace splitStringToRevers method from Main to Revers
+	*  */
+	public static String splitStringToRevers(String str) {
+		final ArrayList<String> words=new ArrayList<String>();
+		Collections.addAll(words, str.split(" "));
+		final ArrayList<String> splitWords=new ArrayList<String>();
+		words.forEach(s-> splitWords.add(Revers.reverseWord(s)));
+		return String.join(" ",splitWords);
+	}
+	private static String reverseWord(String word) {		//change the return type and modifier
 		char[] wordCharArray = word.toCharArray(); //change name from charWord to wordCharArray
 		int startPoint = 0;
 		int endPoint = wordCharArray.length - 1;
