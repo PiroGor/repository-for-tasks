30aed0bf353dfdf3018d12d76ce8a2acbb785318
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-25 15:09:57 +0100

    update Main.java

--- textRevers/src/main/java/app/textRevers/Main.java
+++ textRevers/src/main/java/app/textRevers/Main.java
@@ -1,11 +1,14 @@
 package app.textRevers;
 
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
-		System.out.println(Reverser.reverseSentence("qwe1rty5u rty888jnm!"));
-
+		System.out.println("Please, enter your sentence: \n");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(Reverser.reverseSentence(scanner.nextLine()));
+		scanner.close();
 	}
 }
