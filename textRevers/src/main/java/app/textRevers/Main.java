42f324a28edf9afe66eebbebd2b9ea6612156f38
Anastasiia Nudha <nudhaa@outlook.com>
2024-01-25 15:48:39 +0100

    make interface in Main.class

--- textRevers/src/main/java/app/textRevers/Main.java
+++ textRevers/src/main/java/app/textRevers/Main.java
@@ -4,11 +4,43 @@ package app.textRevers;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-
+	private static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Please, enter your sentence: \n");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(Reverser.reverseSentence(scanner.nextLine()));
-		scanner.close();
+		choicerOfProgram();
 	}
+
+	private static void choicerOfProgram() {
+
+			System.out.println("* * * * * * * * * * * * * * * * * *\n" +
+					"* SELECT WHICH PROGRAM TO EXECUTE *\n" +
+					"*---------------------------------*\n" +
+					"* 1 - Anagrams                    *\n" +
+					"* 2 - IntegerDivision			  *\n" +
+					"*								  *\n" +
+					"* # - Any symbol to exit          *\n" +
+					"* * * * * * * * * * * * * * * * * *\n" +
+					"> ");
+
+			String choice=scanner.nextLine();
+
+			if(choice.equals("1")){
+				reverser();
+			}else if(choice.equals("2")){
+
+			}else{
+				System.out.println("Shutting down...");
+				scanner.close();
+			}
+
+	}
+
+	private static void reverser(){
+		System.out.println( "* * * * * * * * * * * * * * * * * *\n" +
+				"* Please, enter your sentence:     " );
+
+		System.out.println( "* Reversed sentence: \n"+
+				"* "+Reverser.reverseSentence(scanner.nextLine())+
+				"\n* * * * * * * * * * * * * * * * * *\n");
+	}
+
 }
